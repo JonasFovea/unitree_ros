ad0e10a393eba04799ce2a7a2447c78a44a72968
Jonas <jonas.grube@fh-bielefeld.de>
2022-09-08 11:23:02 +0200

    paramInit() commented

--- unitree_controller/src/body.cpp
+++ unitree_controller/src/body.cpp
@@ -8,32 +8,43 @@ Use of this source code is governed by the MPL-2.0 license, see LICENSE.
 namespace unitree_model {
 
 ros::Publisher servo_pub[12];
-unitree_legged_msgs::LowCmd lowCmd;
-unitree_legged_msgs::LowState lowState;
+unitree_legged_msgs::LowCmd lowCmd; //LowCmd message provided by the unitree_ros_to_real package
+unitree_legged_msgs::LowState lowState; //LowState message provided by the unitree_ros_to_real package
 
+
+/**
+ * Function to initialize the MotorCmd messages in the LowCmd message.
+ * Three consecutive motor commands correspond to the three joints of a leg.
+ */
 // These parameters are only for reference.
 // Actual patameters need to be debugged if you want to run on real robot.
 void paramInit()
 {
+    // The LowCmd contains an array of 20 MotorCmd messages
+    // Loop over 4 groups of 3 MotorCmd messages
     for(int i=0; i<4; i++){
-        lowCmd.motorCmd[i*3+0].mode = 0x0A;
-        lowCmd.motorCmd[i*3+0].Kp = 70;
-        lowCmd.motorCmd[i*3+0].dq = 0;
-        lowCmd.motorCmd[i*3+0].Kd = 3;
-        lowCmd.motorCmd[i*3+0].tau = 0;
+        // CMD 0
+        lowCmd.motorCmd[i*3+0].mode = 0x0A; // motor target mode
+        lowCmd.motorCmd[i*3+0].Kp = 70;     // motor spring stiffness coefficient
+        lowCmd.motorCmd[i*3+0].dq = 0;      // motor target velocity
+        lowCmd.motorCmd[i*3+0].Kd = 3;      // motor damper coefficient
+        lowCmd.motorCmd[i*3+0].tau = 0;     // motor target torque
+        // CMD 1
         lowCmd.motorCmd[i*3+1].mode = 0x0A;
         lowCmd.motorCmd[i*3+1].Kp = 180;
         lowCmd.motorCmd[i*3+1].dq = 0;
         lowCmd.motorCmd[i*3+1].Kd = 8;
         lowCmd.motorCmd[i*3+1].tau = 0;
+        // CMD 2
         lowCmd.motorCmd[i*3+2].mode = 0x0A;
         lowCmd.motorCmd[i*3+2].Kp = 300;
         lowCmd.motorCmd[i*3+2].dq = 0;
         lowCmd.motorCmd[i*3+2].Kd = 15;
         lowCmd.motorCmd[i*3+2].tau = 0;
     }
+    //Loop over all MotorCmd messages to set the q value to the corresponding q value of the LowState
     for(int i=0; i<12; i++){
-        lowCmd.motorCmd[i].q = lowState.motorState[i].q;
+        lowCmd.motorCmd[i].q = lowState.motorState[i].q; // motor target position
     }
 }
 
